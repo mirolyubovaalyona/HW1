ce8a0773bf6d4cadd6a4da1e406e62a6b8271c43
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2020-01-31 20:09:40 +1000

    +

--- Page1.xaml.cs
+++ Page1.xaml.cs
@@ -12,9 +12,12 @@ namespace HW1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
+        public string[] Monkeys { get; set; }
         public Page1()
         {
             InitializeComponent();
+            Monkeys = new string[] { "iPhone 8", "Samsung Galaxy S9", "Huawei P10", "LG G6" };
+            this.BindingContext = this;
         }
     }
 }
